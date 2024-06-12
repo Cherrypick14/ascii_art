83cd681e4f025e51712f162ed7ecfbafbec6e423
Cherrypick14 <cherrypicklet@gmail.com>
2024-06-12 07:31:35 +0300

    Add expected value to test

--- ascii/ascii_test.go
+++ ascii/ascii_test.go
@@ -15,6 +15,7 @@ func TestAsciiArt(t *testing.T) {
 	type testCases struct {
 		name string
 		args parameters
+		expected string
 	}
 	// We read the bannerfile directly to save on space/make the code readable
 	// otherwise we would have provided the contents in banner file as a []string.
@@ -25,8 +26,8 @@ func TestAsciiArt(t *testing.T) {
 	textArt := strings.Split(string(contents), "\n")
 
 	tests := []testCases{
-		{name: "test empty", args: parameters{words: []string{}, contents2: textArt}},
-		{name: "test newline(\n)", args: parameters{words: []string{""}, contents2: textArt}},
+		{name: "test empty", args: parameters{words: []string{}, contents2: textArt}, expected:"" },
+		{name: "test newline(\n)", args: parameters{words: []string{""}, contents2: textArt}, expected: },
 		{name: "test Non ascii", args: parameters{words: []string{"你好"}, contents2: textArt}},
 		{name: "test HeL10#\n'", args: parameters{words: []string{"'HeL10#'", ""}, contents2: textArt}},
 		{name: "test {3Hello !@There}", args: parameters{words: []string{"{3Hello !@There}"}, contents2: textArt}},
@@ -36,7 +37,12 @@ func TestAsciiArt(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			AsciiArt(tt.args.words, tt.args.contents2)
+			actual := AsciiArt(tt.args.words, tt.args.contents2)
+
+			if actual != tt.expected {
+				t.Errorf("Expected different output, got: %s", actual)
+			}
+
 		})
 	}
 }
