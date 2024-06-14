a886d9914e5ab9a0be8d35f9a4cea59fcf1c9910
Cherrypick14 <cherrypicklet@gmail.com>
2024-06-14 19:39:41 +0300

    Add expected param

--- ascii/ascii_test.go
+++ ascii/ascii_test.go
@@ -13,8 +13,8 @@ func TestAsciiArt(t *testing.T) {
 		contents2 []string
 	}
 	type testCases struct {
-		name string
-		args parameters
+		name     string
+		args     parameters
 		expected string
 	}
 	// We read the bannerfile directly to save on space/make the code readable
@@ -23,22 +23,23 @@ func TestAsciiArt(t *testing.T) {
 	if err != nil {
 		fmt.Print("Error reading from file", err)
 	}
+	new_Expected, err := os.ReadFile("expected.txt")
+	if err != nil {
+		fmt.Println("Error reading from file", err)
+	}
+
 	textArt := strings.Split(string(contents), "\n")
 
 	tests := []testCases{
-		{name: "test empty", args: parameters{words: []string{}, contents2: textArt}, expected:"" },
-		{name: "test newline(\n)", args: parameters{words: []string{""}, contents2: textArt}, expected: },
-		{name: "test Non ascii", args: parameters{words: []string{"你好"}, contents2: textArt}},
-		{name: "test HeL10#\n'", args: parameters{words: []string{"'HeL10#'", ""}, contents2: textArt}},
-		{name: "test {3Hello !@There}", args: parameters{words: []string{"{3Hello !@There}"}, contents2: textArt}},
-		{name: "test Hello\nThere", args: parameters{words: []string{"Hello", "There"}, contents2: textArt}},
-		{name: "test Hello\n\nThere", args: parameters{words: []string{"Hello", "", "There"}, contents2: textArt}},
+		{name: "test empty", args: parameters{words: []string{}, contents2: textArt}, expected: ""},
+		{name: "test newline(\n)", args: parameters{words: []string{""}, contents2: textArt}, expected: ""},
+		{name: "test Non ascii", args: parameters{words: []string{"你好"}, contents2: textArt}, expected: "Error: Input contains non-ASCII characters"},
+		{name: "test hello", args: parameters{words: []string{"hello"}, contents2: textArt}, expected: string(new_Expected)},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			actual := AsciiArt(tt.args.words, tt.args.contents2)
-
 			if actual != tt.expected {
 				t.Errorf("Expected different output, got: %s", actual)
 			}
